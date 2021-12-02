15cb6eb46320707a32694a143276419f81a08ba9
Kyntaz <1260pedroquintas@gmail.com>
2021-12-02 05:30:40 +0000

    Day 2 done!

--- d2/d2.pl
+++ d2/d2.pl
@@ -1,5 +1,77 @@
 :- ["common/util.pl"].
 
-p1 :- writeln("Solution to part 1").
+/** 
+ * line_to_command(+Line:string, -Command:(string, number)) is semidet
+*/
+line_to_command(Line, (Command, Value)) :-
+    split_string(Line, " ", " ", [Command, ValueStr]),
+    number_string(Value, ValueStr).
 
-p2 :- writeln("Solution to part 2").
+/** 
+ * read_commands(-Commands:list[(string, number)]) is semidet
+*/
+read_commands(Commands) :-
+    read_non_empty_lines(Lines),
+    maplist(line_to_command, Lines, Commands).
+
+/** 
+ * do_command(
+ *      +Command:(string, number),
+ *      +State:(number, number),
+ *      -NewState:(number, number)
+ * ) is semidet
+*/
+do_command(("forward", Value), (Position, Depth), (Position1, Depth)) :- Position1 is Position + Value.
+do_command(("down", Value), (Position, Depth), (Position, Depth1)) :- Depth1 is Depth + Value.
+do_command(("up", Value), (Position, Depth), (Position, Depth1)) :- Depth1 is Depth - Value.
+
+/** 
+ * do_commands(
+ *      +Commands:list[(string, number)],
+ *      +State:(number, number),
+ *      -FinalState(number, number)
+ * ) is semidet
+*/
+do_commands([], State, State).
+do_commands([Command | Commands], State, FinalState) :-
+    do_command(Command, State, State1),
+    do_commands(Commands, State1, FinalState).
+
+/** 
+ * do_command2(
+ *      +Command:(string, number),
+ *      +State:(number, number, number),
+ *      -NewState:(number, number, number)
+ * ) is semidet
+*/
+do_command2(("forward", Value), (Position, Depth, Aim), (Position1, Depth1, Aim)) :-
+    Position1 is Position + Value,
+    Depth1 is Depth + Aim * Value.
+do_command2(("down", Value), (Position, Depth, Aim), (Position, Depth, Aim1)) :-
+    Aim1 is Aim + Value.
+do_command2(("up", Value), (Position, Depth, Aim), (Position, Depth, Aim1)) :-
+    Aim1 is Aim - Value.
+
+/** 
+ * do_commands2(
+ *      +Commands:list[(string, number)],
+ *      +State:(number, number, number),
+ *      -FinalState(number, number, number)
+ * ) is semidet
+*/
+do_commands2([], State, State).
+do_commands2([Command | Commands], State, FinalState) :-
+    do_command2(Command, State, State1),
+    do_commands2(Commands, State1, FinalState).
+
+p1 :-
+    read_commands(Commands),
+    do_commands(Commands, (0,0), (Position, Depth)),
+    N is Depth * Position,
+    writeln(N).
+
+p2 :-
+    read_commands(Commands),
+    do_commands2(Commands, (0,0,0), (Position, Depth, _)),
+    N is Depth * Position,
+    writeln(N).
---
+++ d3/d3.pl
@@ -0,0 +1,5 @@
+:- ["common/util.pl"].
+
+p1 :- writeln("Part one solution").
+
+p2 :- writeln("Part two solution").
