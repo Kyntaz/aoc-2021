32baf0e227d0d7489ed27d805556793ae85f7b77
Kyntaz <1260pedroquintas@gmail.com>
2021-12-06 05:58:17 +0000

    Day 6 done!

--- d6/d6.pl
+++ d6/d6.pl
@@ -1,5 +1,48 @@
 :- use_module("common/util.pl").
 
-p1 :- true.
+/** 
+ * count(+List, -Count)
+*/
+count(List, N, Count) :-
+    (aggregate(count, N^(member(N, List)), Count) ; Count = 0).
 
-p2 :- true.
+/** 
+ * read_lanternfish(-Counts)
+*/
+read_lanternfish(Counts) :-
+    read_non_empty_lines([Line]),
+    split_string(Line, ",", "", StrtList),
+    maplist(number_string, List, StrtList),
+    numlist(0, 8, DaysList),
+    maplist(count(List), DaysList, Counts).
+
+/** 
+ * update_lanternfish(+Lanternfish, -NewLanternfish)
+*/
+update_lanternfish(Lanternfish, NewLanternfish) :-
+    append([[Zero], Other, [Six, Seven, Eight]], Lanternfish),
+    NewSix is Zero + Seven,
+    NewSeven is Eight,
+    NewEight is Zero,
+    append([Other, [Six, NewSix, NewSeven, NewEight]], NewLanternfish).
+
+/** 
+ * reproduce_for_days(+Days, +Lanternfish, -NewLanternfish)
+*/
+reproduce_for_days(0, NewLanternfish, NewLanternfish) :- !.
+reproduce_for_days(Days, Lanternfish, NewLanternfish) :-
+    update_lanternfish(Lanternfish, TempLanternfish),
+    Days1 is Days - 1,
+    reproduce_for_days(Days1, TempLanternfish, NewLanternfish).
+
+p1 :-
+    read_lanternfish(Lanternfish),
+    reproduce_for_days(80, Lanternfish, NewLanternfish),
+    sum_list(NewLanternfish, NLanternfish),
+    writeln(NLanternfish).
+
+p2 :-
+    read_lanternfish(Lanternfish),
+    reproduce_for_days(256, Lanternfish, NewLanternfish),
+    sum_list(NewLanternfish, NLanternfish),
+    writeln(NLanternfish).
