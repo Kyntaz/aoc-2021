1c504e66ab4ec36ff8e89307da1ac8bb331a22a0
Kyntaz <1260pedroquintas@gmail.com>
2021-12-09 05:55:39 +0000

    Day 9 done!

---
+++ d9/d9.pl
@@ -0,0 +1,70 @@
+:- use_module("common/util.pl").
+
+read_heightmap(Heightmap) :-
+    read_grid(Grid),
+    maplist(maplist(char_number), Grid, Heightmap).
+
+get_point(Heightmap, (X, Y), Value) :-
+    nth0(Y, Heightmap, Line),
+    nth0(X, Line, Value).
+
+adjacent((X, Y), (X1, Y)) :-
+    X1 is X + 1.
+adjacent((X, Y), (X1, Y)) :-
+    X1 is X - 1.
+adjacent((X, Y), (X, Y1)) :-
+    Y1 is Y + 1.
+adjacent((X, Y), (X, Y1)) :-
+    Y1 is Y - 1.
+
+point_in_heightmap(Heightmap, (X, Y)) :-
+    nth0(0, Heightmap, Line),
+    length(Heightmap, H),
+    length(Line, W),
+    H1 is H - 1,
+    W1 is W - 1,
+    numlist(0, H1, Ys),
+    numlist(0, W1, Xs),
+    member(X, Xs),
+    member(Y, Ys).
+
+low_point(Heightmap, Point, Value) :-
+    point_in_heightmap(Heightmap, Point),
+    get_point(Heightmap, Point, Value),
+    findall(Adj, (
+        adjacent(Point, Adj),
+        point_in_heightmap(Heightmap, Adj)
+    ), Adjacents),
+    maplist(get_point(Heightmap), Adjacents, Values),
+    maplist(<(Value), Values).
+
+expand_basin(Heightmap, Basin, CompleteBasin) :-
+    findall(Adj, (
+        member(Point, Basin),
+        adjacent(Point, Adj),
+        get_point(Heightmap, Adj, V),
+        V =\= 9
+    ), Adjacents),
+    append([Basin, Adjacents], NewBasinUnsrt),
+    sort(NewBasinUnsrt, NewBasin),
+    (NewBasin = Basin -> CompleteBasin = NewBasin ; expand_basin(Heightmap, NewBasin, CompleteBasin)).
+
+find_basin(Heightmap, Point, Basin) :-
+    expand_basin(Heightmap, [Point], Basin).
+
+p1 :- 
+    read_heightmap(Heightmap),
+    findall(Value, low_point(Heightmap, _, Value), Lowpoints),
+    maplist(calc(+, 1), Lowpoints, RiskLevels),
+    sum_list(RiskLevels, TotalRisk),
+    writeln(TotalRisk).
+
+p2 :-
+    read_heightmap(Heightmap),
+    findall(Point, low_point(Heightmap, Point, _), Lowpoints),
+    maplist(find_basin(Heightmap), Lowpoints, Basins),
+    maplist(length, Basins, BasinSizes),
+    msort(BasinSizes, SortedSizes),
+    append([_, [V1, V2, V3]], SortedSizes),
+    V is V1 * V2 * V3,
+    writeln(V).
