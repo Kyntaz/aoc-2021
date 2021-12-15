7246cc511b6db311bbcd659937f564ca8f3240b6
Kyntaz <1260pedroquintas@gmail.com>
2021-12-15 23:08:24 +0000

    Add multiple answers and run stats

--- run.ps1
+++ run.ps1
@@ -1,4 +1,4 @@
 $day = $args[0]
 $part = $args[1]
 echo "Running $part of $day"
-Get-Content "$day/input.txt" | swipl -q -O -g "$part." "$day/$day.pl"
+Get-Content "$day/input.txt" | swipl -q -g "time(($part, fail))." "$day/$day.pl"
